bf04f5de42f0a0f4ba3955b6bab6125fbcf03cdc
Chase 李 <chase@acm.cs.uic.edu>
2021-09-07 00:52:35 -0500

    Adding boilerplate form example. Updating default component prefixes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
 
 // Angular Material Components
 import {MatToolbarModule} from '@angular/material/toolbar'; 
@@ -12,11 +14,14 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
 import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,8 +31,14 @@ import {MatStepperModule} from '@angular/material/stepper';
     MatIconModule,
     MatButtonModule,
     MatButtonToggleModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
